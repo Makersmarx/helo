ac837ae250e945d5b474246e0e9ab02ed5d7e4b1
Makersmarx <bbiehn1@gmail.com>
2020-08-17 16:12:26 -0600

    more server details and routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
-import React from 'react';
-import Auth from './components/Auth/Auth.js';
-import Form from './components/Form/Form.js';
+import React, { Component } from 'react';
+import routes from './routes.js';
 import Nav from './components/Nav/Nav.js';
-import Post from './components/Post/Post.js';
-import Dashboard from './components/Dashboard/Dashboard.js';
 import './App.css';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Auth />
-        <Form />
         <Nav />
-        <Post />
-        <Dashboard />
+        {routes}
       </div>
     );
   }
