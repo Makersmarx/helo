64c8ecd52e4518a5834a797fa91d8e19571fd7c4
Makersmarx <bbiehn1@gmail.com>
2020-08-17 15:41:33 -0600

    basic server setup connected to db

---
+++ server/index.js
@@ -0,0 +1,38 @@
+// .dotenv
+require('dotenv').config();
+
+// port being used
+const port = 8001;
+
+//express
+const express = require('express');
+
+const app = express();
+
+// massive
+const massive = require('massive');
+
+// controller
+// const controller = require('./controller')
+
+// destructure vars from .env to use in index.js
+const { SERVER_PORT, CONNECTION_STRING } = process.env;
+
+// massive connecting to db
+massive({
+  connectionString: CONNECTION_STRING,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+})
+  .then((db) => {
+    app.set('db', db);
+    console.log('db connected');
+  })
+  .catch((err) => console.log(err));
+
+app.use(express.json);
+
+// endpoints
+
+app.listen(port, () => console.log(`Loud and Proud on Port: ${port}`));
