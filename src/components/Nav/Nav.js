a79e50fe71a11bdf64956165fd7a61831650ff19
Makersmarx <bbiehn1@gmail.com>
2020-08-17 16:51:05 -0600

    nav buttons linked

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Nav extends React.Component {
   render() {
     return (
-      <div>
-        <p>Nav</p>
+      <div className="nav-bar">
+        <img
+          className="profile-pic"
+          src="https://robohash.org/why"
+          alt="profile"
+        />
+        <nav className="nav-links">
+          <Link to="/dashboard">
+            <button className="nav-btn">Home</button>
+          </Link>
+          <Link to="/new">
+            <button className="nav-btn">New Post</button>
+          </Link>
+          <Link to="/">
+            <button className="nav-btn">Logout</button>
+          </Link>
+        </nav>
       </div>
     );
   }
